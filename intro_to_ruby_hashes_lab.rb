b61258c417260626f73bc0f86f46d944fff4d052
Seth Wax <sethlwax@gmail.com>
2018-03-30 16:08:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,13 +12,28 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads =>
-		{:pieces => 4, :names => {}, :rent_in_dollars => {}}
+		{
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
+		}
 	}
 end
 
 def monopoly_with_third_tier
 	monopoly = {:railroads =>
-		{:pieces => 4, :names => {}}}
+		{
+			:pieces => 4,
+			:names =>
+				{
+
+				},
+			:rent_in_dollars =>
+				{
+					:one_piece_owned => 25
+				}
+		}
+	}
 end
 
 def monopoly_with_fourth_tier
